05a0a4799aa8960030264ab0e7c4bb59d72b453f
DiTotto <luca.ditotto99@gmail.com>
2024-05-28 15:31:38 +0200

    .

---
+++ utils/load_win.sh
@@ -0,0 +1,3 @@
+docker cp ..\spark\query1.py spark-master:/opt/spark/work-dir/query1.py
+docker cp ..\spark\query2.py spark-master:/opt/spark/work-dir/query2.py
+docker cp ..\spark\query3.py spark-master:/opt/spark/work-dir/query3.py
