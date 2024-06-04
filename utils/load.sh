0b88daf4a8356aad328736f7d5d77128dd214331
DiTotto <luca.ditotto99@gmail.com>
2024-06-04 09:41:32 +0200

    .

--- utils/load.sh
+++ utils/load.sh
@@ -3,3 +3,6 @@
 docker cp ../spark/query1.py spark-master:/opt/spark/work-dir/query1.py
 docker cp ../spark/query2.py spark-master:/opt/spark/work-dir/query2.py
 docker cp ../spark/query3.py spark-master:/opt/spark/work-dir/query3.py
+docker cp ../spark/query1SQL.py spark-master:/opt/spark/work-dir/query1SQL.py
+docker cp ../spark/query2SQL.py spark-master:/opt/spark/work-dir/query2SQL.py
+docker cp ../spark/query3SQL.py spark-master:/opt/spark/work-dir/query3SQL.py
